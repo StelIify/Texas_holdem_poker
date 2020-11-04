176e32be37da9c2510c133036458b328cfcf7521
ozorenko <olexandr.zorenko@o2.sk>
2020-11-04 16:57:52 +0100

    implementation of finding the best card in the hand so far: high card, two pair, three of a kind, straight

--- poker/hand.py
+++ poker/hand.py
@@ -1,6 +1,53 @@
 class Hand():
     def __init__(self, cards):
-        self.cards = cards
+        copy = cards[:]
+        copy.sort()
+        self.cards = copy
+
+    @property
+    def _rank_validation_from_best_to_worst(self):
+        return (
+            ("Straight", self._is_straight()),
+            ("Three of a Kind", self._is_three_of_a_kind()),
+            ("Two Pair", self._is_two_pair()),
+            ("Pair", self._is_pair()),
+            ("High Card", self._is_high_card())
+        )
+
+    def best_card(self):
+        for rank in self._rank_validation_from_best_to_worst:
+            name, validate_func = rank
+            if validate_func:
+                return name
+
+    def _is_straight(self):
+        if len(self.cards) < 5:
+            return False
+
+        rank_indexes = [card.rank_index for card in self.cards]
+        return rank_indexes == list(range(rank_indexes[0], rank_indexes[-1] + 1))
+
+    def _is_three_of_a_kind(self):
+        three_of_a_kind = self._ranks_with_count(3)
+        return len(three_of_a_kind) == 1
+
+    def _is_two_pair(self):
+        rank_with_pairs = self._ranks_with_count(2)
+        return len(rank_with_pairs) == 2
+
+    def _is_pair(self):
+        rank_with_pairs = self._ranks_with_count(2)
+        return len(rank_with_pairs) == 1
+
+    def _is_high_card(self):
+        return True
+
+    def _ranks_with_count(self, count):
+        return {
+            rank: rank_count
+            for rank, rank_count in self._card_rank_count.items()
+            if rank_count == count
+        }
 
     @property
     def _card_rank_count(self):
@@ -9,27 +56,3 @@ class Hand():
             card_rank_count.setdefault(card.rank, 0)
             card_rank_count[card.rank] += 1
         return card_rank_count
-
-    def best_card(self):
-        rank_with_pairs = {
-            rank: rank_count
-            for rank, rank_count in self._card_rank_count.items()
-            if rank_count == 2
-        }
-
-        if len(rank_with_pairs) == 2:
-            return "Two Pair"
-        elif len(rank_with_pairs) == 1:
-            return "Pair"
-        else:
-            return "High Card"
-
-    # @staticmethod
-    # def count_rank_occurrence(cards):
-    #     counts = {}
-    #     for card in cards:
-    #         if card.rank in counts:
-    #             counts[card] += 1
-    #         else:
-    #             counts[card] = 1
-    #     return counts
