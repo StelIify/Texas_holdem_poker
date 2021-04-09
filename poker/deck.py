7662538e0782bb4111f7bc9c0ad2a942cccd08af
Oleks Zorenko <olexandr.zorenko@o2.sk>
2021-04-09 12:56:12 +0200

    Added two methods, for shuffling and dealing cards

--- poker/deck.py
+++ poker/deck.py
@@ -1,7 +1,22 @@
+import random
+
+
 class Deck():
     def __init__(self):
         self.cards = []
 
-    def add_card(self, cards):
+    def __len__(self):
+        return len(self.cards)
+
+    def add_cards(self, cards):
         self.cards.extend(cards)
 
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def deal_cards(self, number_of_cards):
+        """Returns a list of cards and deletes them from
+        self.cards attribute"""
+        cards = self.cards[:number_of_cards]
+        del self.cards[:number_of_cards]
+        return cards
--- tests/test_deck.py
+++ tests/test_deck.py
@@ -14,8 +14,24 @@ class DeckTest(unittest.TestCase):
     def test_add_card_to_collection(self):
         deck = Deck()
         card = Card("Ace", "Clubs")
-        deck.add_card([card])
+        deck.add_cards([card])
         self.assertEqual(
             deck.cards,
             [card]
         )
+
+    def test_deck_has_length(self):
+        deck = Deck()
+        self.assertEqual(len(deck), 0)
+
+    def test_remove_specified_number_of_cards_from_deck(self):
+        ace = Card("Ace", "Hearts")
+        king = Card("King", "Clubs")
+        queen = Card("Queen", "Diamonds")
+        cards = [ace, king, queen]
+
+        deck = Deck()
+        deck.add_cards(cards)
+
+        self.assertEqual(deck.deal_cards(2), [ace, king])
+        self.assertEqual(deck.cards, [queen])
